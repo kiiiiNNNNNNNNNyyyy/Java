96e32b8c3e535957f4b94756e61cff9f2dd1e338
kiiiiNNNNNNNNNyyyy <ad.sam007@gmail.com>
2017-01-30 17:40:22 -0500

    Addition program

---
+++ Basic Programs/src/Add.java
@@ -0,0 +1,9 @@
+public class Add{
+	public static void main(String args[]){
+		System.out.println("This Program prints some basic text.");
+		System.out.println("************************************\n");
+		System.out.println("Welcome to Java.");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun.");
+	}
+}
