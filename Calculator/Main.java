7e8be4cc4aadd6f1772177493add8af082a5c480
Arjun  Dass <ad.sam007@gmail.com>
2017-09-02 20:35:26 -0500

    Added array and switch statements

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -1,22 +1,40 @@
 public class Main{
     public static void main(String args[]){
-        double val1 = 100;
-        double val2 = 50.0;
-        double result;
-        char opCode = 'a';
 
-        if(opCode == 'a'){
-            result = val1 + val2;
-        }else if(opCode == 's'){
-            result = val1 - val2;
-        }else if(opCode == 'd'){
-            result = val1/val2;
-        }else if(opCode == 'm'){
-            result = val1 * val2; 
-        }else{
-            result = 0.0;
+        double[] leftVals ={100.0d, 25.0d, 224.3d, 11.0d};
+        double[] rightVals ={100.0d, 25.0d, 224.3d, 11.0d};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
+
+        // double val1 = 100;
+        // double val2 = 50.0;
+        // double result;
+        // char opCode = 'a';
+
+        for(int i=0; i<opCodes.length; i++){
+            switch(opCodes[i]){
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'd':
+                    results[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
+                    break;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                default:
+                    System.out.println("Invalid Code!!");
+                    results[i] = 0.0;
+                    break;
+            }
         }
 
-        System.out.println(result);
+        for(double result: results){
+            System.out.println(result);
+        }
+        
     }
 }
