269b3fb936945722d064b0f7c063529c3ee48dbf
Arjun  Dass <ad.sam007@gmail.com>
2017-09-02 21:08:36 -0500

    Calculator fucked

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -1,40 +1,29 @@
 public class Main{
     public static void main(String args[]){
 
-        double[] leftVals ={100.0d, 25.0d, 224.3d, 11.0d};
-        double[] rightVals ={100.0d, 25.0d, 224.3d, 11.0d};
-        char[] opCodes = {'d', 'a', 's', 'm'};
-        double[] results = new double[opCodes.length];
+        // double[] leftVals ={100.0d, 25.0d, 224.3d, 11.0d};
+        // double[] rightVals ={100.0d, 25.0d, 224.3d, 11.0d};
+        // char[] opCodes = {'d', 'a', 's', 'm'};
+        // double[] results = new double[opCodes.length];
 
-        // double val1 = 100;
-        // double val2 = 50.0;
-        // double result;
-        // char opCode = 'a';
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = create(100.0d, 50.0, 'd');
+        equations[0] = create(100.0d, 50.0, 'a');
+        equations[0] = create(100.0d, 50.0, 's');
+        equations[0] = create(100.0d, 50.0, 'm');
 
-        for(int i=0; i<opCodes.length; i++){
-            switch(opCodes[i]){
-                case 'a':
-                    results[i] = leftVals[i] + rightVals[i];
-                    break;
-                case 's':
-                    results[i] = leftVals[i] - rightVals[i];
-                    break;
-                case 'd':
-                    results[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
-                    break;
-                case 'm':
-                    results[i] = leftVals[i] * rightVals[i];
-                    break;
-                default:
-                    System.out.println("Invalid Code!!");
-                    results[i] = 0.0;
-                    break;
-            }
+        for(MathEquation equation: equations){
+            equation.execute();
+            System.out.println(equation.result);
         }
+    }
 
-        for(double result: results){
-            System.out.println(result);
-        }
-        
+    public static MathEquation create(double leftVal, double rightVal, char opCode){
+        MathEquation equation = new MathEquation();
+        equation.leftVal = leftVal;
+        equation.rightVal = rightVal;
+        equation.opCode = opCode;
+
+        return equation;
     }
 }
