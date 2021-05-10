5b8194c4dd593765f7f4620864b438df6c6307d2
Arjun Dass <ad.sam007@gmail.com>
2021-05-10 14:50:32 -0400

    threads interrupt and join

--- Concurrency/src/AnotherThread.java
+++ Concurrency/src/AnotherThread.java
@@ -7,7 +7,8 @@ public class AnotherThread extends Thread {
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
-            System.out.println(ANSI_BLUE + " Another thread woke me up!");
+            System.out.println(ANSI_BLUE + "Another thread woke me up!");
+            return;
         }
 
         System.out.println(ANSI_BLUE + "Three seconds have passed and I'm awake!!");
--- Concurrency/src/Main.java
+++ Concurrency/src/Main.java
@@ -20,9 +20,17 @@ public class Main {
             @Override
             public void run() {
                 System.out.println(ANSI_RED + "Hello from the anonymous class's runnable implementation");
+                try {
+                    anotherThread.join(2000);
+                    System.out.println(ANSI_RED + "Another thread terminated or timed out, so I am running again..");
+                } catch (InterruptedException e) {
+                    System.out.println(ANSI_RED + "I couldn't wait after all, I was interrupted");
+                }
             }
         });
         myRunnableThread.start();
+
+        // anotherThread.interrupt();
         System.out.println("Hello again from the main thread!!");
     }
 }
